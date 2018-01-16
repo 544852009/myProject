6003b2bffc0be35b3712f99c2fe0b726f602ef4e
544852009 <35055991+544852009@users.noreply.github.com>
2018-01-16 13:20:29 +0800

    1

--- src/main/java/com/controller/LoginController.java
+++ src/main/java/com/controller/LoginController.java
@@ -1,13 +1,17 @@
 package com.controller;
 
-import com.google.gson.JsonParser;
+import com.entity.Userinfo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import javax.xml.registry.infomodel.User;
 
 @Controller
 @RequestMapping(value = "/my", method = RequestMethod.GET)
@@ -19,10 +23,13 @@ public class LoginController {
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String printHello(HttpServletRequest request, HttpServletResponse response, Model model) {
+    public String printHello(@Valid Userinfo userinfo, BindingResult bingingresult , HttpServletRequest request, HttpServletResponse response, Model model) {
+        if(bingingresult.hasErrors()){
+            System.out.println(bingingresult.getModel());
+        }
+        String  ss = userinfo.getUsername();
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         return "success";
     }
-    JsonParser jsonParser = new JsonParser();
 }
